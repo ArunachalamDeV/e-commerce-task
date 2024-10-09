dcee06bcdcd370c3b41aa861799c4b969934064a
ArunachalamDeV <arunachalamuthumariappan@gmail.com>
2024-10-09 12:36:10 +0530

    Update middleWare.js

--- e-commerce/backend/src/middleWare.js
+++ e-commerce/backend/src/middleWare.js
@@ -1,12 +1,12 @@
 // middlewares/authMiddleware.js
 const jwt = require('jsonwebtoken');
-
+const jwtSecret = 'your_jwt_secret_key';
 // Protect Middleware
 const authenticate = (req, res, next) => {
         const token = req.headers['authorization'];
         if (!token) return res.status(403).jon({status:"error",isAuth:false});
         try {
-          const decoded = jwt.verify(token.split(' ')[1], process.env.jwtSecret);
+          const decoded = jwt.verify(token.split(' ')[1],jwtSecret);
           req.user = decoded;      
           next();
         } catch (err) {

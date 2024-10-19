b4299d2e1afbb337defb590138e6cd4a9883f76d
ArunachalamDeV <arunachalamuthumariappan@gmail.com>
2024-10-20 05:01:29 +0530

    s

--- e-commerce/backend/handler.js
+++ e-commerce/backend/handler.js
@@ -3,6 +3,7 @@ const serverless = require("serverless-http");
 require('dotenv').config()
 const app = require("./src/app")()
 
+
 process.env.DEPLOYMENT
   ? (module.exports.app = serverless(app))
   : app.listen(3333, () => console.log("server running on port 3333"));
